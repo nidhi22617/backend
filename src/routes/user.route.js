e75d98e6fedf75779573a9dbbe5b5d4df7e476a4
nidhi22617 <pnidhi2@hotmail.com>
2024-08-19 00:00:32 +0545

    fix access and refresh Token

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,14 @@
 import { Router} from "express";
-import {registerUser} from "../controllers/user.controller.js"
+import {
+        refreshAccessToken,
+        loginUser, 
+        logoutUser, 
+        registerUser,
+        } from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+
 const router = Router()
  
 
@@ -16,6 +24,12 @@ router.route("/register").post(
         }
     ]),
     registerUser)
+
+router.route("/login").post(loginUser)
+
+// secured routes
+router.route("/logout").post(verifyJWT,  logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 
 export default router
