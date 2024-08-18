3701212c6b6a15ae187fafdbb36b4fa58c809a61
nidhi22617 <pnidhi2@hotmail.com>
2024-08-18 13:54:19 +0545

    all fix user register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,72 +6,83 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 
 
 
-const registerUser = asyncHandler(async (req, res) => {
+const registerUser = asyncHandler( async (req, res) => {
   // get user details from frontend
   // validation - not empty
   // check if user already exists: username, email
   // check for images, check for avatar
-  // upload them to cloudinary - avatar
+  // upload them to cloudinary, avatar
   // create user object - create entry in db
   // remove password and refresh token field from response
   // check for user creation
-  // return response
+  // return res
 
 
-  const {fullName, email, username, password} = req.body
-  console.log("fullName", fullName);
-  
-  
+  const {fullName, email, username, password } = req.body
+  //console.log("email: ", email);
+
   if (
-    [fullName, email, username, password].some((field) => field?.trim() === "")
-  ){
-    throw new ApiError(400, "All fields are required")
-    }
-const existedUser = User.findOne({
-  $or: [{ username },{ email }]
-})
+      [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+      throw new ApiError(400, "All fields are required")
+  }
 
-if (existedUser) {
-    throw new ApiError(409,"User with email or username already exists");
-      
-}
+  const existedUser = await User.findOne({
+      $or: [{ username }, { email }]
+  })
 
-const avatarLocalPath = req.files?.avatar[0]?.path
-const  coverImageLocalPath = req.files?.coverImage[0]?.path
+  if (existedUser) {
+      throw new ApiError(409, "User with email or username already exists")
+  }
+  //console.log(req.files);
 
-if (!avatarLocalPath) {
-    throw new ApiError(400, "Avatar file is required");
-    
-}
+  // const avatarLocalPath = req.files?.avatar[0]?.path || null;
+  let avatarLocalPath;
+  if (req.files && Array.isArray(req.files.avatar) && req.files.avatar.length > 0) {
+    avatarLocalPath =  req.files.avatar[0].path
+  }
+  //const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
+  let coverImageLocalPath;
+  if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+      coverImageLocalPath = req.files.coverImage[0].path
+  }
+  
 
-const avatar = await uploadOnCloudinary(avatarLocalPath)
-const coverImage = await uploadOnCloudinary (coverImageLocalPath)
+  if (!avatarLocalPath) {
+      throw new ApiError(400, "Avatar file is required");
+  }
 
-if (!avatar) {
-  throw new ApiError(400, "Avatar file is required")
-}
-const user = await User.create({
-  fullName,
-  avatar: avatar.url,
-  coverImage: coverImage?.url || "",
-  email,
-  password,
-  username: username.toLowerCase()
-})
+  const avatar = await uploadOnCloudinary(avatarLocalPath)
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
-const createdUser = await User.findById(user._id).select(
-  "-password-refreshToken"
-)
+  if (!avatar) {
+      throw new ApiError(400, "Avatar file is required");
+  }
+ 
 
-if (!createdUser) {
-    throw new ApiError("Something went wrong while registring user");
-    
-}
+  const user = await User.create({
+      fullName,
+      avatar: avatar.url,
+      coverImage: coverImage?.url || "",
+      email, 
+      password,
+      username: username.toLowerCase()
+  })
 
-return res.status(201).json(
-  new ApiResponse(200, createdUser, "User registered Successfully")
-)
-})
+  const createdUser = await User.findById(user._id).select(
+      "-password -refreshToken"
+  )
+
+  if (!createdUser) {
+      throw new ApiError(500, "Something went wrong while registering the user")
+  }
+
+  return res.status(201).json(
+      new ApiResponse(200, createdUser, "User registered Successfully")
+  )
+
+} )
+
 
 export { registerUser };
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -2,7 +2,7 @@ class ApiError extends Error {
     constructor (
         statusCode,
         message = "Something went wrong",
-        error = [],
+        errors = [],
         stack = ""
     ){
         super(message)
@@ -10,7 +10,7 @@ class ApiError extends Error {
         this.data = null
         this.message = message
         this.success = false;
-        this.errors = errors
+        this.errors = this.errors
 
         if (stack) {
             this.stack = stack
@@ -19,4 +19,5 @@ class ApiError extends Error {
         }
     }
 }
-export {ApiError}
+
+export { ApiError }
